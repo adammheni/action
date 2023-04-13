e035c16607bcbf90c64e5755c48ecd6113fb8a96
adammheni <ademdmmhenni@gmail.com>
2023-04-13 14:04:35 +0100

    modified:   id/src/main/java/com/app/id/IdApplication.java

--- id/src/main/java/com/app/id/IdApplication.java
+++ id/src/main/java/com/app/id/IdApplication.java
@@ -2,10 +2,16 @@ package com.app.id;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
 @SpringBootApplication
 public class IdApplication {
-
+@GetMapping("/welcome")
+	public String welcome()
+	{
+		return"welcome to main";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(IdApplication.class, args);
 	}
